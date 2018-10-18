919a50e50075b23fa6fccf2d187de46c3c936667
S St <simon.steuernagel@web.de>
2018-10-18 16:22:38 +0200

    Updated research, tested subscriber (AnyMsg)

--- src/subTest.py
+++ src/subTest.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import rospy
+import rostopic
 from std_msgs.msg import String
 
 
 def callback(data):
     # rospy.loginfo(rospy.get_caller_id() + "RECEIVED: %s", data.data)
     rospy.loginfo("Received something")
+
 
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -15,7 +17,11 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
+
     rospy.Subscriber("chatter", String, callback)
+    # can use msg type "rospy.msg.AnyMsg" to subscribe to any topic you want, but the resulting data is useless
+    #   since it is not deserialized
+    rospy.Subscriber("tracked_objects/scan", rospy.msg.AnyMsg, callback)
     # rospy.Subscriber("tracked_objects/scan", bob_perception_msgs/TrackedLaserScan/cbf82c106cc765795116663d337cea66, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
