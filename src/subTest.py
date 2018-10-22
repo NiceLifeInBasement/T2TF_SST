ffe769572f5e2ebf61c5ce83dbff52f87ba32e0e
S St <simon.steuernagel@web.de>
2018-10-22 15:22:23 +0200

    Version that displays tracking point data from the bagfile for a single tracked object based on its id

--- src/subTest.py
+++ src/subTest.py
@@ -3,12 +3,39 @@ import rospy
 import rostopic
 from std_msgs.msg import String
 from bob_perception_msgs.msg import *
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
-
-
+uid = 194773  # set to 0 if you want to select a uid from the first data frame
 def callback(data):
-    # rospy.loginfo(rospy.get_caller_id() + "RECEIVED: %s", data.data)
-    rospy.loginfo("Received something")
+    # data is the received data, in the format described by the msg file
+    box_array = data.boxes  # extract the array of tracked obj boxes
+
+    # Take the first unique object id that you find in the data, and track its x|y across the entire bag
+
+    global uid  # global variable for tracking the uid across all function calls
+    if uid == 0:
+        # no uid checked for yet
+        uid = box_array[2].object_id  # select the tracked uid
+    pos = -1  # reset the position counter
+    for x in range(0, len(box_array)):
+        if box_array[x].object_id == uid:
+            pos = x
+    if not pos == -1:
+        # found the uid in the callback data
+        # print its x coordinate
+        coord_str = str(box_array[pos].box.center_x) + "|" + str(box_array[pos].box.center_y)
+        rospy.loginfo("UID "+str(uid)+" currently has Position: "+coord_str)
+
+        # The following is only necessary to plot the point data
+        global ax, fig
+        ax.plot(box_array[pos].box.center_x, box_array[pos].box.center_y, 'ro')
+        plt.show()
+        # Showed the point in the plot
+    else:
+        rospy.loginfo("UID "+str(uid)+" not found in this callback")
 
 
 def listener():
@@ -27,8 +54,15 @@ def listener():
     rospy.Subscriber("tracked_objects/scan", TrackedLaserScan, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    # rospy.spin()
 
 
 if __name__ == '__main__':
     listener()
+
+    # The following is only necessary to plot the point data
+    # Currently all painted dots are shown, this causes overlapping and cluttering of the graph
+    global ax, fig
+    fig = plt.figure()
+    ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
+    plt.show()
