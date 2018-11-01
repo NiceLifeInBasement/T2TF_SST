91df99dfeda88d7f665a311e501f08f619a5a744
S St <simon.steuernagel@web.de>
2018-11-01 13:01:05 +0100

    added information and extraction support for the maven-1.bag pc

--- src/general/pc2_publisher.py
+++ src/general/pc2_publisher.py
@@ -10,14 +10,55 @@ from collections import namedtuple
 import sys
 import subprocess
 from bob_perception_msgs.msg import *
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
 import sensor_msgs.point_cloud2 as pc2
+import std_msgs.msg
+
+
+def callback_modded(data):
+    global pub, xoff, yoff
+    divide = True  # Just a flag for me
+    multi = 1  # Multiply all values by this
+
+    # ---- #
+    # Actual Function
+    point_list = list(pc2.read_points(data.point_cloud, skip_nans=True, field_names=("x", "y", "z")))
+    print("Acquired a List of points. Length: "+str(len(point_list))+" || Entry0: "+str(point_list[0]))
+
+    # Currently trying to normalize the data in the array to the position of one of the entries
+    # Struggling with the pc2 -> array -> pc2 conversion though
+    # Probably a waste of time anyway, since 90-110pts is not enough to display any meaningful data
+    # and the maven-X.bag files have only that amount of points.
+    # In general, the fact that the points are at x/y-values like x.e+07 is kinda weird
+    # According to some quick research, rviz has some known issues with this kind of size of values
+
+    if len(point_list)>0 and xoff == 0:
+        xoff = point_list[0][0]
+    if len(point_list) > 0 and yoff == 0:
+        yoff = point_list[0][1]
+    redone_list = []
+    min_x = 999999999999999999
+    max_x = -min_x
+    for t in point_list:
+        if divide:
+            new_t = ((t[0]-xoff) * multi,(t[1]-yoff) * multi, t[2])
+        else:
+            new_t = (t[0], t[1], t[2])
+        redone_list.append(new_t)
+        if new_t[0] < min_x:
+            min_x = new_t[0]
+        if new_t[0] > max_x:
+            max_x = new_t[0]
+    print("\tmin_x: "+str(min_x)+" / max_x: "+str(max_x))
+
+    c = pc2.create_cloud(data.point_cloud.header, data.point_cloud.fields, redone_list)
+    # c = data.point_cloud
+    pub.publish(c)
 
 
 def callback(data):
+    # Simply publish the pc2 data without any modifications at all to the channel
     global pub
-    point_list = list(pc2.read_points(data.point_cloud, skip_nans=True, field_names=("x", "y", "z")))
-    print("Acquired a List of points. Length: "+str(len(point_list)))
     pub.publish(data.point_cloud)
 
 
@@ -25,13 +66,16 @@ def republisher(topic_name):
     global pub
     rospy.init_node('laser_scan_extractor', anonymous=True)
     pub = rospy.Publisher(topic_name, PointCloud2, queue_size=100)
-    rospy.Subscriber("tracked_objects/scan", TrackedLaserScan, callback)
+    rospy.Subscriber("tracked_objects/scan", TrackedLaserScan, callback_modded)
 
     # and now start spinning so that it doesnt stop
     rospy.spin()
 
 
 if __name__ == '__main__':
+    global xoff, yoff
+    xoff = 0
+    yoff = 0
     topic_name = "pc2data"
     if len(sys.argv) > 1:
         bag_name = sys.argv[1]
